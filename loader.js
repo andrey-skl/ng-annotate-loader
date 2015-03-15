4036701a7eceff8893a6c5e6f39dcf281fb63240
Kent C. Dodds <kent+github@doddsfamily.us>
2015-03-14 21:13:52 -0600

    Adding sourcemaps and caching

--- loader.js
+++ loader.js
@@ -1,6 +1,28 @@
 var ngAnnotate = require('ng-annotate');
+var utils = require('loader-utils');
+var SourceMapConsumer = require('source-map').SourceMapConsumer;
+var SourceMapGenerator = require('source-map').SourceMapGenerator;
 
-module.exports = function(content) {
-  var annotated =  ngAnnotate(content, {add: true});
-  return annotated.src;
+module.exports = function(source, sm) {
+  this.cacheable && this.cacheable();
+  var filename = utils.getCurrentRequest(this);
+
+  var res = ngAnnotate(source, {
+      add: true,
+      sourcemap: {inline: false, inFile: filename, sourceRoot: filename}
+    }
+  );
+
+  var mergeMap;
+  if (sm) {
+    var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(sm));
+    if (res.map) {
+      generator.applySourceMap(new SourceMapConsumer(res.map), filename);
+      mergeMap = generator.toString();
+    } else {
+      mergeMap = sm;
+    }
+  }
+
+  this.callback(null, res.src || source, mergeMap);
 };
