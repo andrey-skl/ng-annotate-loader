c1d5471bfcc5a71c4631d5e208a93c28d74cc469
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2015-06-09 01:07:00 +0300

    Support configuration via query params

--- loader.js
+++ loader.js
@@ -3,18 +3,32 @@ var utils = require('loader-utils');
 var SourceMapConsumer = require('source-map').SourceMapConsumer;
 var SourceMapGenerator = require('source-map').SourceMapGenerator;
 
-module.exports = function(source, sm) {
-  this.cacheable && this.cacheable();
-  var filename = utils.getCurrentRequest(this);
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
 
-  var res = ngAnnotate(source, {
-      add: true,
-      sourcemap: {inline: false, inFile: filename, sourceRoot: filename}
+function getOptions(sourceMapEnabled) {
+    var defaults = { add: true };
+
+    var options = this.query ? utils.parseQuery(this.query) : defaults;
+
+    if (sourceMapEnabled && options.sourcemap === undefined) {
+        var filename = utils.getCurrentRequest(this);
+        options.sourcemap = {inline: false, inFile: filename, sourceRoot: filename};
     }
-  );
+
+    return options;
+}
+
+module.exports = function(source, sm) {
+  var sourceMapEnabled = this.sourceMap;
+  this.cacheable && this.cacheable();
+
+  var res = ngAnnotate(source, getOptions.call(this, sourceMapEnabled));
 
   var mergeMap;
-  if (sm) {
+
+  if (sourceMapEnabled && sm) {
     var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(sm));
     if (res.map) {
       generator.applySourceMap(new SourceMapConsumer(res.map), filename);
