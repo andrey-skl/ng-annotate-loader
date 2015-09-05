5842d7d97300c11e24df445eca6f2d9b1d3b9be2
John V McCarthy <jvm@hp.com>
2015-09-05 14:33:07 -0600

    Use loader context resourcePath to reference the filename for the source map, ensuring the original source is referenced.

--- loader.js
+++ loader.js
@@ -22,7 +22,6 @@ function getOptions(sourceMapEnabled, filename) {
         options.map = {
           inline: false, 
           inFile: filename, 
-          sourceRoot: filename
         };
     }
 
@@ -37,7 +36,7 @@ function getOptions(sourceMapEnabled, filename) {
 module.exports = function(source, inputSourceMap) {
   var outputSourceMap;
   var sourceMapEnabled = this.sourceMap;
-  var filename = utils.getCurrentRequest(this);
+  var filename = this.resourcePath;
   this.cacheable && this.cacheable();
 
   var annotateResult = ngAnnotate(source, getOptions.call(this, sourceMapEnabled, filename));  
@@ -46,16 +45,14 @@ module.exports = function(source, inputSourceMap) {
   //   https://github.com/babel/babel/blob/d3a73b87e9007104cb4fec343f0cfb9e1c67a4ec/packages/babel/src/transformation/file/index.js#L465
   // See also vinyl-sourcemaps-apply (used by gulp-ng-annotate) - https://github.com/floridoo/vinyl-sourcemaps-apply/blob/master/index.js
   if (sourceMapEnabled && inputSourceMap) {    
-    if (annotateResult.map) {
-      var annotateMap = JSON.parse(annotateResult.map);
-      //Sources array should be filled somehow to work with source-map package
-      annotateMap.sources[0] = filename;
+    if (annotateResult.map) {     
+      var annotateMap = JSON.parse(annotateResult.map);      
       
       var generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(annotateMap));
-      generator.applySourceMap(new SourceMapConsumer(inputSourceMap));
+      generator.applySourceMap(new SourceMapConsumer(inputSourceMap), filename);
       
       outputSourceMap = generator.toJSON();
-      outputSourceMap.sources = inputSourceMap.sources;
+      
       //Should be set to avoid '../../file is not in SourceMap error https://github.com/huston007/ng-annotate-loader/pull/11'
       outputSourceMap.sourceRoot = '';
       //Copy file name from incoming file because it is empty by some unknown reaon
